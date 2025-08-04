3e7b3c2cbe8b743f43f520953d538cc0e86e5f5b
Best0Ich <trifonkozurak@gmail.com>
2025-08-04 17:49:34 +0300

    Create new project

---
+++ test_json.py
@@ -0,0 +1,20 @@
+
+entry = {
+    "title": "Продукты",
+    "entries": [
+        {
+            "title": "Мясное",
+            "entries": [
+                {
+                    "title": "Колбаса",
+                    "entries": [
+                        {
+                            "title": "Салями",
+                            "entries": []
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
